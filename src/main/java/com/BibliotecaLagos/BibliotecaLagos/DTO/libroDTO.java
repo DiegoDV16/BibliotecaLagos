b4fab9e97bd80b5c3fad948380f42b3a23220829
DiegoDV16 <diegosoto.vd@gmail.com>
2026-05-18 17:47:09 -0400

    feat
    feat: agregado controller, service y repository de libros

---
+++ src/main/java/com/BibliotecaLagos/BibliotecaLagos/DTO/libroDTO.java
@@ -0,0 +1,20 @@
+package com.BibliotecaLagos.BibliotecaLagos.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class libroDTO {
+
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private String editorial;
+    private Integer anioPublicacion;
+    private Integer cantidadTotal;
+    private Integer categoriaId;
+}
